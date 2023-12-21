a1e30aabf71a855b936ce73cf92d7b3d6a0a5401
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-12-22 02:00:22 +0300

    iteration

--- samples/minimal/build.ts
+++ samples/minimal/build.ts
@@ -1,6 +1,6 @@
 import { boot, transform, exit } from "npm:imgit/server";
 
-await boot();
+await boot({ width: 720 });
 const content = await Deno.readTextFile("./index.html");
 const html = await transform(content);
 await Deno.writeTextFile("./public/index.html", html);
