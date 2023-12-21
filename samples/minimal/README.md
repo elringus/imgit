a1896a82198797809bd5658f07f0d84e82caeb67
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-12-21 20:02:43 +0300

    sampling

---
+++ samples/minimal/README.md
@@ -0,0 +1,7 @@
+﻿# Minimal Sample
+
+Example on using imgit with plain HTML and vite.
+
+- Run `min:dev` npm script to run developer server
+- Run `min:build` to build for production
+- Run `min:preview` to preview production build
---
+++ samples/minimal/vite.config.ts
@@ -0,0 +1,8 @@
+﻿import { defineConfig } from "vite";
+import imgit from "imgit/vite";
+import youtube from "imgit/youtube";
+import svg from "imgit/svg";
+
+export default defineConfig({
+    plugins: [imgit({ root: "../assets", plugins: [youtube(), svg()] })]
+});
