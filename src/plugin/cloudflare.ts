2b2dcbcc8bfbc8c2db5b3a6d775be31acce3c2ad
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-12-21 17:35:23 +0300

    testing

--- src/plugin/cloudflare.ts
+++
@@ -1,5 +0,0 @@
-import { EncodedAsset } from "../server/asset.js";
-
-export default function (credentials: unknown): (path: string, asset: EncodedAsset) => Promise<string> {
-    return async (path, asset) => "";
-}
