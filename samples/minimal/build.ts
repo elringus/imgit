63ce7f0d52d42ec6ba01fc3c403962f0c54df8cf
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-12-22 18:57:15 +0300

    sampling

--- samples/minimal/build.ts
+++ samples/minimal/build.ts
@@ -1,13 +1,31 @@
 import { boot, transform, exit } from "npm:imgit/server";
 
-// Configure and initialize imgit server to load cached results
-// from previous runs. In this case we're setting width threshold
+// Configure imgit server. In this case we're setting width threshold
 // to 800px, so that when content is larger it'll be scaled down,
 // while high-res original will still be shown on high-dpi displays.
+// Will as well load cached results from previous runs on boot.
 await boot({ width: 800 });
 
-
+// Read sample HTML file with remote images and video referenced
+// via markdown image tags: ![](url). The format can be changed
+// in boot config, for example to capture custom JSX tags instead.
 const content = await Deno.readTextFile("./index.html");
+
+// Run the imgit transformations over sample HTML.
+// This will capture referenced content syntax, fetch the remote files,
+// encode them to AV1/AVIF, generate covers, dense and safe variants
+// when necessary, serve generated files to a CDN (in this minimal case
+// the files will be copied to 'public' directory instead) and return
+// transformed content where captured content syntax is replaced with
+// <picture> and <video> HTML tags referencing generated files.
 const html = await transform(content);
+
+// Write the transformed HTML under 'public' directory.
 await Deno.writeTextFile("./public/index.html", html);
+
+// Shutdown imgit server. Will cache the results of the transform operations,
+// such as probing results, encoding profiles, generated covers and parameters
+// of the source files. On consequent runs the server will skip most operations
+// in case the cache is valid and source file is not modified. Cache files are
+// written under 'public/imgit' directory (can be changed in boot config).
 await exit();
