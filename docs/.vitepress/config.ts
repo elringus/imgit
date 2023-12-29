00999a09826f5e440ca7081abcf3ad7fda022b64
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-12-29 21:21:54 +0300

    remove api

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -35,7 +35,6 @@ export default defineConfig({
         docFooter: { prev: "Previous page", next: "Next page" },
         nav: [
             { text: "Guide", link: "/guide/introduction", activeMatch: "/guide/" },
-            { text: "Reference", link: "/api/" },
             {
                 text: `v${(await import("./../../package.json")).version}`, items: [
                     { text: "Changes", link: "https://github.com/elringus/imgit/releases/latest" },
